7ae4951c7a4babdae01674407dfc51469fe73a90
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MBP.lan>
2025-03-08 06:51:41 -0800

    Added date for when the blog was written. Added the ability to display the date. Added the ability to cast the date and parse such that 2025-03-07 would read March 7th, 2025

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	def ordinal_suffix(day)
+		suffix = case day % 10
+				 when 1 then 'ST'
+				 when 2 then 'ND'
+				 when 3 then 'RD'
+				 else 'TH'
+				end
+		"<span class='ordinal-suffix'>#{suffix}</span>".html_safe
+	end
+
 end
