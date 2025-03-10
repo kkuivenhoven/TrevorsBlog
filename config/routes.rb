56de3552b9e9288c236bcb82cc70077beeef7efe
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MBP.lan>
2025-03-09 20:20:47 -0700

    Changed root page to About page

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "pages#home"
+  root "pages#about"
 end
