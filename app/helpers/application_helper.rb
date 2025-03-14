cf904891d3f91e9387be199ede38b81a504e6490
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MBP.lan>
2025-03-13 20:06:42 -0700

    Changed the musty pink out for musty green

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,8 @@
 module ApplicationHelper
 
-=begin
 	def ordinal_suffix(day)
 	  suffix = day.ordinalize[-2..-1] # Get the suffix (st, nd, rd, th)
 	  "<span class='ordinal'>#{suffix}</span>" # Wrap the suffix in a span with a class
 	end
-=end
 
 end
