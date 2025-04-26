c9ff906d73226b4f5588fbaf518e2e59c861b3b8
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MBP.lan>
2025-04-26 13:02:34 -0700

    Added further functionality to the decision tree helper function for titleizing a title. I updated the decision tree case name

--- app/helpers/decision_tree_helper.rb
+++ app/helpers/decision_tree_helper.rb
@@ -2,7 +2,13 @@ module DecisionTreeHelper
 
 	def format_file_title(file)
 		file_name = File.basename(file, ".*")
-		file_name.tr("_", " ").titleize
+		formatted_name = file_name.tr("_", " ").titleize
+
+		if formatted_name.downcase.include?("mcdonalds")
+			formatted_name.gsub!(/McDonald/i, "McDonald'")
+		end
+
+		formatted_name
 	end
 
 end
