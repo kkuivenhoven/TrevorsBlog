d577bfbcecc6c9bec72752e9ff506a435020e573
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MBP.lan>
2025-05-01 17:02:12 -0700

    Updated the header on the index page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,7 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import * as bootstrap from "bootstrap"
+import Rails from '@rails/ujs'
+
+// Turbo.session.drive = false // Optional: Disable Turbo for specific links or forms if needed.
+Rails.start()
