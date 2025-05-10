3cd6a14b74b7d7a3cea9daa95ff199b8b4870090
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MBP.lan>
2025-05-10 08:00:58 -0700

    Removing confirmable from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_02_001457) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_10_145920) do
   create_table "fraud_prompts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "category"
@@ -29,13 +29,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_02_001457) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.integer "query_count"
     t.datetime "last_query_at"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
