1aec5a41fc3e33e313b382412f69f2c77bb338aa
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MBP.lan>
2025-05-13 20:45:38 -0700

    Updated the action mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { 
-	host: "www.trevorbalthrop.com", 
+	host: "mail.trevorbalthrop.com", 
 	protocol: "https" 
   }
 
