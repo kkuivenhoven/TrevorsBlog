c757b9f6614457340c1ac2781c5e8c5c7fead7b3
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MBP.lan>
2025-05-22 20:07:54 -0700

    Added posts to the admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,6 +4,23 @@ class AdminController < ApplicationController
   
   def dashboard
 	# any data you want to show
+    # List all available JSON files
+	@posts = Dir.glob(Rails.root.join('app/assets/blog_posts/*.json')).map do |f|
+	  # Read the file and parse the JSON
+	  file_content = File.read(f)
+	  json_data = JSON.parse(file_content)
+
+	  # Extract the title and content
+	  {
+		title: json_data['title'],
+		content: json_data['content'],
+		date: Date.parse(json_data['date_published']),
+		file_name: File.basename(f, '.json') # Get the file name without the extension
+	  }
+	end
+
+	@posts = @posts.sort_by { |post| post[:date] }.reverse
+
   end
 
   private
