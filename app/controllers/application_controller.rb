3745cdc379851f829702b0da85b3f6e4eaf27e11
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MBP.lan>
2025-06-01 16:02:45 -0700

    Added application controller method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  BAD_IPS = [
+    '114.119.135.207',
+    '114.119.151.155',
+    # Add other suspicious IPs or IP ranges here
+  ]
+
+  def block_bad_ips
+	if BAD_IPS.include?(request.remote_ip)
+		head :forbidden
+	end
+  end
 
   # Redirect after sign-out
   def after_sign_out_path_for(resource_or_scope)
