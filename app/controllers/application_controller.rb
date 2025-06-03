22be629afa78110425e9d5921af30d3ac14588b6
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MBP.lan>
2025-06-02 18:41:55 -0700

    Added before action call for restricting non usa ips

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :restrict_non_usa_ips
   require 'ipaddr'
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
