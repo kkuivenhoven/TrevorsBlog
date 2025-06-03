27493b18f602aa849957e3a761905e832edb7567
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MBP.lan>
2025-06-02 17:32:40 -0700

    Fixed a small typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,10 @@
 class ApplicationController < ActionController::Base
   require 'ipaddr'
-  before_action :block_bad_ips, :restrict_non_usa_ips
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  BAD_IP_RANGES = [
-	IPAddr.new('114.119.0.0/16'), # Huawei Cloud Service - China - suspicious bot activity
-	IPAddr.new('103.136.220.0/24') # HostBangla - Bangladesh - suspicious bot activity
-	# Can add more CIDR ranges or specific IPs id needed
-  ]
-
-  def block_bad_ips
-	remote_ip = IPAddr.new(request.remote_ip)
-
-	if BAD_IP_RANGES.any? { |ip_range| ip_range.include?(remote_ip) }
-		head :forbidden
-	end
-  end
-
   def restrict_non_usa_ips
-	fowarded_ips = request.headers['X-Forwarded-For'];
+	frowarded_ips = request.headers['X-Forwarded-For'];
 	user_ip = if forwarded_ips.present?
 				user_ip = forwarded_ips.split(',').first.strip
 			  else
