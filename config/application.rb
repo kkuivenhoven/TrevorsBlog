6b0a72ade6c9511a633ddbd18e88d4f7c6a797bd
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MBP.lan>
2025-06-07 06:31:04 -0700

    Added module of Middleware and Middleware reference in config file

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,6 @@ module TrevorsBlog20
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-	config.middleware.insert_before 0, GeoIpUsOnly
+	config.middleware.insert_before 0, Middleware::GeoIpUsOnly
   end
 end
--- lib/middleware/geo_ip_us_only.rb
+++ lib/middleware/geo_ip_us_only.rb
@@ -1,23 +1,25 @@
-class GeoIpUsOnly
+module Middleware
+	class GeoIpUsOnly
 
-	def initialize(app)
-		@app = app
-	end
+		def initialize(app)
+			@app = app
+		end
 
-	def call(env)
-		req = Rack::Request.new(env)
-		ip = (req.get_header('HTTP_X_FORWARDED_FOR') || req.ip).split(',').first
+		def call(env)
+			req = Rack::Request.new(env)
+			ip = (req.get_header('HTTP_X_FORWARDED_FOR') || req.ip).split(',').first
 
-		location = Geocoder.search(ip).first
-		country = location ? location.country_code : nil
-	
-		puts "[GeoIP Debug IP: #{ip}, Country: #{country}"
-		Rails.logger.info "[GeoIP Debug IP: #{ip}, Country: #{country}"
+			location = Geocoder.search(ip).first
+			country = location ? location.country_code : nil
+		
+			puts "[GeoIP Debug IP: #{ip}, Country: #{country}"
+			Rails.logger.info "[GeoIP Debug IP: #{ip}, Country: #{country}"
 
-		if country == 'US'
-			@app.call(env)
-		else
-			[404, { 'Content-Type' => 'text/html' }, ['Not Found']]
+			if country == 'US'
+				@app.call(env)
+			else
+				[404, { 'Content-Type' => 'text/html' }, ['Not Found']]
+			end
 		end
 	end
 end
