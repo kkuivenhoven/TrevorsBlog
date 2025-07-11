687d3ec718a54cf0918a76df552b6e46b525cd87
Kendra Kuivenhoven <kendrakvnhvn10@gmail.com>
2025-07-11 16:35:53 -0700

    Reject the sources and images if there isn't any

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -2,6 +2,10 @@ class BlogPost < ApplicationRecord
 	has_many :blog_post_images, dependent: :destroy
 	has_many :blog_post_sources, dependent: :destroy
 
-	accepts_nested_attributes_for :blog_post_images, allow_destroy: true
-	accepts_nested_attributes_for :blog_post_sources, allow_destroy: true
+	accepts_nested_attributes_for :blog_post_images, 
+					  allow_destroy: true
+					, reject_if: ->(attrs) { attrs['image_url'].blank? && attrs['image'].blank? }
+	accepts_nested_attributes_for :blog_post_sources, 
+					  allow_destroy: true
+					, reject_if: ->(attrs) { attrs['source_url'].blank? }
 end
