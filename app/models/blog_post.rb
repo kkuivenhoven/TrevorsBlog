b475ecb532a08b66f21e92223179428313f3cbaa
Kendra Kuivenhoven <kendrakvnhvn10@gmail.com>
2025-07-11 16:41:35 -0700

    Fixed syntax

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -3,9 +3,9 @@ class BlogPost < ApplicationRecord
 	has_many :blog_post_sources, dependent: :destroy
 
 	accepts_nested_attributes_for :blog_post_images, 
-					  allow_destroy: true
-					, reject_if: ->(attrs) { attrs['image_url'].blank? && attrs['image'].blank? }
+					  allow_destroy: true, 
+					  reject_if: ->(attrs) { attrs['image_url'].blank? && attrs['image'].blank? }
 	accepts_nested_attributes_for :blog_post_sources, 
-					  allow_destroy: true
-					, reject_if: ->(attrs) { attrs['source_url'].blank? }
+					  allow_destroy: true, 
+					  reject_if: ->(attrs) { attrs['source_url'].blank? }
 end
