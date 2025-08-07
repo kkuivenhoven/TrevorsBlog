c5eb24b238ade04abb097d608f66d44fc696f10a
Kendra Kuivenhoven <kendrakvnhvn10@gmail.com>
2025-08-06 18:37:42 -0700

    Fixed reference to wrong flag

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -14,7 +14,7 @@ class BlogPostsController < ApplicationController
   end
 
   def show
-	if @blog_post.visible == false
+	if @blog_post.is_visible == false
 		redirect_to blog_post_path
 	end
   end
