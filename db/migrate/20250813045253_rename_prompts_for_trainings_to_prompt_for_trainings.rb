93bacc3a1bad0cbf3455cf0435f48eef2639c733
Kendra Kuivenhoven <kendrakvnhvn10@gmail.com>
2025-09-26 19:43:50 -0700

    Added debugging statements..

--- db/migrate/20250813045253_rename_prompts_for_trainings_to_prompt_for_trainings.rb
+++ db/migrate/20250813045253_rename_prompts_for_trainings_to_prompt_for_trainings.rb
@@ -1,7 +1,9 @@
 class RenamePromptsForTrainingsToPromptForTrainings < ActiveRecord::Migration[8.0]
   def change
 	if table_exists?(:prompts_for_trainings)
+		puts "Table prompts_for_Trainings exists. Renaming..."
 		rename_table :prompts_for_trainings, :prompt_for_trainings
+		puts "Rename complete."
 	else
 		puts "Table prompts_for_trainings does not exist, skipping rename"
 	end
