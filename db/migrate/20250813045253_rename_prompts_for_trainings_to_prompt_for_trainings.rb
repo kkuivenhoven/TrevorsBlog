849bd00635d8889069313484ab747fa60c78dc10
Kendra Kuivenhoven <kendrakvnhvn10@gmail.com>
2025-09-26 19:38:50 -0700

    Adjusted the rename DB file

--- db/migrate/20250813045253_rename_prompts_for_trainings_to_prompt_for_trainings.rb
+++ db/migrate/20250813045253_rename_prompts_for_trainings_to_prompt_for_trainings.rb
@@ -1,5 +1,9 @@
 class RenamePromptsForTrainingsToPromptForTrainings < ActiveRecord::Migration[8.0]
   def change
-	rename_table :prompts_for_trainings, :prompt_for_trainings
+	if table_exists?(:prompts_for_trainings)
+		rename_table :prompts_for_trainings, :prompt_for_trainings
+	else
+		puts "Table prompts_for_trainings does not exist, skipping rename"
+	end
   end
 end
